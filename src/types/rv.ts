019912655b1620cb0dba685673199230940d109a
jhou <v-jhou@expedia.com>
2026-08-17 20:31:25 -0700

    Update rv.ts

--- src/types/rv.ts
+++ src/types/rv.ts
@@ -1,1 +1,20 @@
-
+export interface RvProfile {
+  rvType: string;
+  heightFeet: number;
+  heightInches: number;
+  lengthFeet: number;
+  combinedLengthFeet: number;
+  weightLbs: number;
+  towingMpg: number | '';
+  towSetup: string;
+  propaneStyle: string;
+  propaneCount: number;
+  propaneLb: number;
+  memberships: string[];
+  ampRating: string;
+  minHookup: 'full' | 'partial' | 'electric' | 'dry' | string;
+  batteryType: string;
+  solarWatts: string;
+  hasDogbone: boolean;
+  hasStarlink: boolean;
+}
